5c8d871b6f7c5e46b0b211ad113a3a69261348db
Leandro <leandrofernandesvieira@gmail.com>
2021-04-20 09:49:48 -0300

    feat: styling welcome screen

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,29 +1,40 @@
-import React, { useState } from "react";
-import { SafeAreaView, Text, Image, StyleSheet } from "react-native";
-
-import { Button } from "../components/Button";
-
-import colors from "../styles/colors";
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 import wateringImg from "../assets/watering.png";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"}
-        suas plantas {"\n"}
-        de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de {"\n"}
+          forma fácil
+        </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
 
-      <Button title=">" />
+        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -31,26 +42,33 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  wrapper: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
   },
 
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: "bold",
+    fontFamily: fonts.heading,
+    lineHeight: 34,
     textAlign: "center",
     color: colors.heading,
     marginTop: 38,
   },
 
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get("window").width * 0.7,
   },
 
   subtitle: {
     textAlign: "center",
     fontSize: 18,
+    fontFamily: fonts.text,
     paddingHorizontal: 20,
     color: colors.heading,
   },
@@ -60,13 +78,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 16,
-    marginBottom: 14,
+    marginBottom: 10,
     height: 56,
     width: 56,
   },
 
-  buttonText: {
+  buttonIcon: {
+    fontSize: 32,
     color: colors.white,
-    fontSize: 24,
   },
 });
