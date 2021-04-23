70cc43c04558169763577c975fff667304c0d425
Leandro <leandrofernandesvieira@gmail.com>
2021-04-23 01:26:27 -0300

    feat: environment selection filter

---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+import api from "../services/api";
+
+import { EnvironmentButton } from "../components/EnvironmentButton";
+import { Header } from "../components/Header";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
+
+export function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api.get("plants_environments");
+      setEnvironments([
+        {
+          key: "all",
+          title: "Todos",
+        },
+        ...data,
+      ]);
+    }
+
+    fetchEnvironment();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
+
+      <View>
+        <FlatList
+          data={environments}
+          renderItem={({ item }) => <EnvironmentButton title={item.title} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.environmentList}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+
+  header: {
+    paddingHorizontal: 32,
+  },
+
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+
+  environmentList: {
+    height: 40,
+    justifyContent: "center",
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
+  },
+});
